0f506a0815506ea36153d91a0b5e71c2bbdb4eb3
Naren <coding.demon.naren@gmail.com>
2024-08-20 00:31:40 +0530

    modified footer

--- client/src/statics/Footer.jsx
+++ client/src/statics/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="bg-zinc-800 text-white p-4 text-center min-h-[10vh] h-[10vh]">
             <p className="text-sm">© 2024 Chat App. All rights reserved.</p>
-            <p className="text-xs mt-2">Designed and developed with ❤️ by Naren</p>
+            <p className="text-xs mt-2">Designed and developed by Naren</p>
         </div>
     );
 }
